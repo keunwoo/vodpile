da617c27e113848622a8ae4ad5e3979e404ad5c3
Keunwoo Lee <keunwoo@gmail.com>
2014-05-11 00:34:45 -0700

    fetch.go: a little main() program to fetch & write channel items

---
+++ twitch/fetch/fetch.go
@@ -0,0 +1,43 @@
+// Fetches listings for a given Twitch channel and outputs the merged JSON to stdout or a file.
+
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/keunwoo/vodpile/twitch"
+)
+
+func main() {
+	twitchChannel := flag.String("channel", "", "Name of Twitch channel to fetch (e.g. 'gsl')")
+	output := flag.String("output", "", "Output file; if absent, prints to stdout.")
+	flag.Parse()
+
+	if *twitchChannel == "" {
+		log.Fatal("Empty -channel argument; cannot fetch.")
+	}
+
+	var enc *json.Encoder
+	if *output == "" {
+		writer := bufio.NewWriter(os.Stdout)
+		enc = json.NewEncoder(writer)
+		defer writer.Flush()
+	} else {
+		log.Printf("Writing to output file: %s", *output)
+		outfile, err := os.Create(*output)
+		if err != nil {
+			panic(err)
+		}
+		defer outfile.Close()
+		enc = json.NewEncoder(outfile)
+	}
+
+	client := &http.Client{}
+	vids := twitch.FetchVideoList(client, *twitchChannel)
+	enc.Encode(vids)
+}
